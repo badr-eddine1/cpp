bba886a25e39ead72bea788644f2029285983c5b
badr eddine <aitbenijjab@gmail.com>
2024-11-05 16:27:35 +0100

    parie2

--- exe1tp3.cpp
+++ exe1tp3.cpp
@@ -44,7 +44,60 @@ void location::afficherInfos(){
     cout<< "l'age est "<<age<<endl;
     cout<< "le contact est "<<contact<<endl;
 }
+class bienLocation{
+    string adress;
+    double montantL;
+    bool occupe;
+    public :
+    bienLocation();
+    bienLocation(string A,double M, bool O);
+    string getAdress(){
+        return adress;
+    }
+    double getM(){
+        return montantL;
+    }
+    bool geto(){
+        return occupe;
+    }
+    void setA(string  ad){
+        adress=ad;
+    }
+    void setM(double m){
+        montantL=m;
+    }
+    void setO(bool occ){
+        occupe=occ;
+    }
+    bool basculerOp();
+    void afficherProp();
+};
+bienLocation::bienLocation(){
+    adress="";
+    montantL=0;
+    occupe=1;
+}
+ bienLocation::bienLocation(string A,double M, bool O){
+    adress=A;
+    montantL=M;
+    occupe=O;
+ }
+bool bienLocation::basculerOp(){
+    if(occupe==1){
+        occupe=0;
+    }
+    else {occupe=1;}
+    return occupe;
+}
+void bienLocation::afficherProp(){
+    cout<< "l'adress est "<<adress<<endl;
+    cout<< "le montant est"<<montantL<<endl;
+    cout<< "l'occupation est "<<occupe<<endl;
+}
 int main(){
     location L;
+    bienLocation b("badr@gmail.com",200,0);
     L.afficherInfos();
+    b.afficherProp();
+
 }
